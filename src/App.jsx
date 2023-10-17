5b8b45f7ba7659f4ff4c6ac0e7cded0061e98cbc
taison <chta22086@g.nihon-u.ac.jp>
2023-10-17 23:01:09 +0900

    ホームページのマークアップ

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import Navi from "./components/Navi";
+import Welcome from "./components/Welcome";
+import ImageSide from "./components/ImageSide";
+
+export default function App() {
+    return (
+      <div>
+         <img src="/src/public/dove.png" />
+      </div>
+    );
+  }
+  
